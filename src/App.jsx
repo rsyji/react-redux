19f9e9b378c89254280cf1fa8e0c4ff5cce244c2
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-08-22 17:23:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import products from "./products.json";
+import Product from "./Components/Product";
+import Cart from "./Components/Cart";
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="App min-w-full">
+      <Cart />
+      <div className="my-8 flex">
+      <div className="products flex justify-center flex-wrap">
+        {products.map((product) => (
+          <Product key={product.id} {...product} />
+        ))}
+      </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
