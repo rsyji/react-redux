35763a9c723cdf30d7b9edc8ee430d7917a0d18b
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-08-22 17:23:08 +0530

    Create Cart.jsx

---
+++ src/Components/Cart.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { getItemsSelector } from "../redux/slices/cartSlice";
+
+const Cart = () => {
+  const items = useSelector(getItemsSelector);
+  const total = items.reduce((a, b) => a + b.price, 0);
+  console.log(items)
+  return (
+    <div className="alert alert-success">
+      <h3 className="text-center bg-blue-600 font-bold text-2xl">
+        Total Items: {items.length} (Rs. {total})/-
+      </h3>
+    </div>
+  );
+};
+
+export default Cart;
